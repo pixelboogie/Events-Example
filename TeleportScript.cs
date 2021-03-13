4ed5c87e9df4e30dc944ee7499e6be8fa746223b
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-12 17:28:39 -0800

    Add files via upload

---
+++ TeleportScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportScript : MonoBehaviour 
+{
+    void OnEnable()
+    {
+        EventManager.OnClix += Teleport;
+    }
+
+
+    void OnDisable()
+    {
+        EventManager.OnClix -= Teleport;
+    }
+
+
+    void Teleport()
+    {
+        Vector3 pos = transform.position;
+        pos.y = Random.Range(1.0f, 3.0f);
+        transform.position = pos;
+    }
+}
---
+++ TurnColorScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnColorScript : MonoBehaviour 
+{
+    void OnEnable()
+    {
+        EventManager.OnClix += TurnColor;
+    }
+
+
+    void OnDisable()
+    {
+        EventManager.OnClix -= TurnColor;
+    }
+
+
+    void TurnColor()
+    {
+        Color col = new Color(Random.value, Random.value, Random.value);
+        GetComponent<Renderer>().material.color = col;
+    }
+}
